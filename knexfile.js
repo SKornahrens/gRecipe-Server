858c5e9c92a215e9b2f93dcc4ecc4164f4a793c1
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-09-14 18:37:23 -0600

    setting up heroku

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,8 @@ module.exports = {
     client: 'pg',
     connection:  'postgre://localhost/test-grecipeAPI'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
